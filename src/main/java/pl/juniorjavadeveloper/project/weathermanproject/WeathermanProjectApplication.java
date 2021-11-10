3d4d49911082bf3ecb432a18f2fef7454c15dda9
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-11-10 13:30:45 +0100

    Add Spring Boot Application main class

---
+++ src/main/java/pl/juniorjavadeveloper/project/weathermanproject/WeathermanProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.juniorjavadeveloper.project.weathermanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathermanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathermanProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/juniorjavadeveloper/project/weathermanproject/WeathermanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.juniorjavadeveloper.project.weathermanproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathermanProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
