a807577527cfb3b42642f19c8dbe50e48be96d7a
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-11-16 21:45:53 +0100

    Add test cases in Unit Test

--- src/test/java/pl/juniorjavadeveloper/project/weathermanproject/service/mapper/LocationMapperTest.java
+++ src/test/java/pl/juniorjavadeveloper/project/weathermanproject/service/mapper/LocationMapperTest.java
@@ -9,18 +9,25 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class LocationMapperTest {
 
+    private static final long LOCATION_ID_1 = 1L;
+    private static final String LOCATION_CITY_WARSAW = "Warsaw";
+
     @Test
     void givenMapper_whenFromModel_thenEntityHasCorrectValues() {
         // given
         LocationMapper locationMapper = new LocationMapper();
         LocationModel locationModel = new LocationModel();
+        locationModel.setId(LOCATION_ID_1);
+        locationModel.setCity(LOCATION_CITY_WARSAW);
 
         // when
         LocationEntity locationEntity = locationMapper.from(locationModel);
 
         // then
         assertAll(
-                () -> assertNotNull(locationEntity, "Mapped locationEntity is null")
+                () -> assertNotNull(locationEntity, "Mapped locationEntity is null"),
+                () -> assertNotNull(locationEntity.getId(), "locationEntity ID is null"),
+                () -> assertNotNull(locationEntity.getCity(), "locationEntity CITY is null")
         );
     }
 
@@ -28,14 +35,18 @@ class LocationMapperTest {
     void givenMapper_whenFromEntity_thenModelHasCorrectValues() {
         // given
         LocationMapper locationMapper = new LocationMapper();
+        LocationEntity locationEntity = new LocationEntity();
+        locationEntity.setId(LOCATION_ID_1);
+        locationEntity.setCity(LOCATION_CITY_WARSAW);
 
         // when
-        LocationEntity locationEntity = new LocationEntity();
         LocationModel locationModel = locationMapper.from(locationEntity);
 
         // then
         assertAll(
-                () -> assertNotNull(locationModel, "Mapped locationModel is null")
+                () -> assertNotNull(locationModel, "Mapped locationModel is null"),
+                () -> assertNotNull(locationModel.getId(), "locationModel ID is null"),
+                () -> assertNotNull(locationModel.getCity(), "locationModel CITY is null")
         );
     }
 }
