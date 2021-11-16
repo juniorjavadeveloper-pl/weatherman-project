21d1df4433fe5cac4e4265ae9a3c9b3e3394286c
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-11-16 14:15:58 +0100

    Add WeathermanService logic implementation

--- src/main/java/pl/juniorjavadeveloper/project/weathermanproject/service/WeathermanService.java
+++ src/main/java/pl/juniorjavadeveloper/project/weathermanproject/service/WeathermanService.java
@@ -3,46 +3,64 @@ package pl.juniorjavadeveloper.project.weathermanproject.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import pl.juniorjavadeveloper.project.weathermanproject.repository.LocationRepository;
 import pl.juniorjavadeveloper.project.weathermanproject.repository.entity.LocationEntity;
 import pl.juniorjavadeveloper.project.weathermanproject.service.mapper.LocationMapper;
 import pl.juniorjavadeveloper.project.weathermanproject.web.model.LocationModel;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WeathermanService {
     private static final Logger LOGGER = LoggerFactory.getLogger(WeathermanService.class);
 
+    private final LocationRepository locationRepository;
     private final LocationMapper locationMapper;
 
-    public WeathermanService(LocationMapper locationMapper) {
+    public WeathermanService(LocationRepository locationRepository, LocationMapper locationMapper) {
+        this.locationRepository = locationRepository;
         this.locationMapper = locationMapper;
     }
 
     // L - list aka. listLocations()
     public List<LocationModel> list() {
         LOGGER.info("list()");
-        return null;
+        List<LocationEntity> locationEntities = locationRepository.findAll();
+        List<LocationModel> locationModels = locationMapper.fromList(locationEntities);
+        LOGGER.info("list() = {}", locationModels);
+        return locationModels;
     }
 
     // C - create aka. addLocation(...)
     public LocationModel create(LocationModel locationModel) {
         LOGGER.info("create({})", locationModel);
         LocationEntity locationEntity = locationMapper.from(locationModel);
-        return null;
+        LocationEntity savedLocationEntity = locationRepository.save(locationEntity);
+        LocationModel savedLocationModel = locationMapper.from(savedLocationEntity);
+        LOGGER.info("create() = {}", savedLocationModel);
+        return savedLocationModel;
     }
 
     // R - read aka. getLocationWeather(...)
     public LocationModel read(Long id) {
         LOGGER.info("read({})", id);
-        return null;
+        Optional<LocationEntity> optionalLocationEntity = locationRepository.findById(id);
+        // FIXME: add Exception handling instead of new LocationEntity()!!!
+        LocationEntity locationEntity = optionalLocationEntity.orElse(new LocationEntity());
+        LocationModel locationModel = locationMapper.from(locationEntity);
+        LOGGER.info("read() = {}", locationModel);
+        return locationModel;
     }
 
     // U - update aka. saveLocationWeather(...)
     public LocationModel update(LocationModel locationModel) {
         LOGGER.info("update({})", locationModel);
         LocationEntity locationEntity = locationMapper.from(locationModel);
-        return null;
+        LocationEntity savedLocationEntity = locationRepository.save(locationEntity);
+        LocationModel savedLocationModel = locationMapper.from(savedLocationEntity);
+        LOGGER.info("update() = {}", savedLocationModel);
+        return savedLocationModel;
     }
 
     // D - delete aka. removeLocation(...)
--- src/main/java/pl/juniorjavadeveloper/project/weathermanproject/service/mapper/LocationMapper.java
+++ src/main/java/pl/juniorjavadeveloper/project/weathermanproject/service/mapper/LocationMapper.java
@@ -7,9 +7,19 @@ import org.springframework.stereotype.Component;
 import pl.juniorjavadeveloper.project.weathermanproject.repository.entity.LocationEntity;
 import pl.juniorjavadeveloper.project.weathermanproject.web.model.LocationModel;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class LocationMapper {
     private static final Logger LOGGER = LoggerFactory.getLogger(LocationMapper.class);
+
+    public List<LocationModel> fromList(List<LocationEntity> locationEntities) {
+        LOGGER.info("from({})", locationEntities);
+        return locationEntities.stream()
+                .map(this::from)
+                .collect(Collectors.toList());
+    }
 
     public LocationEntity from(LocationModel locationModel) {
         LOGGER.info("from({})", locationModel);
