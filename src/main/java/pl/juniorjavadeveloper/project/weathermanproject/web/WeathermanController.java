66e90e84f6e512b0b4f2be85d49beed18eb63e41
Junior Java Developer Mentor <kontakt@juniorjavadeveloper.pl>
2021-11-25 08:26:17 +0100

    Add html view for adding new location along with endpoints in Controller

--- src/main/java/pl/juniorjavadeveloper/project/weathermanproject/web/WeathermanController.java
+++ src/main/java/pl/juniorjavadeveloper/project/weathermanproject/web/WeathermanController.java
@@ -1,6 +1,8 @@
 package pl.juniorjavadeveloper.project.weathermanproject.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.juniorjavadeveloper.project.weathermanproject.service.WeathermanManagerService;
 
@@ -13,18 +15,31 @@ public class WeathermanController {
         this.weathermanManagerService = weathermanManagerService;
     }
 
-    // L - list
-    public void list() {
+    // L - list aka. listLocations()
+    @GetMapping
+    public String listView() {
+        return "locations";
     }
-    // C - create
-    public void create() {
+
+    // C - create aka. addLocation(...)
+    @GetMapping
+    public String createView() {
+        return "location-add";
     }
+
+    @PostMapping
+    public String create() {
+        return "locations";
+    }
+
     // R - read
     public void read() {
     }
+
     // U - update
     public void update() {
     }
+
     // D - delete
     public void delete() {
     }
